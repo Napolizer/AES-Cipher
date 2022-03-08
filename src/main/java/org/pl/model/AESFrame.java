509fd850d4c4d33f21bf86c87de49fb13d4800be
napolizer <napolizer@op.pl>
2022-03-08 12:27:14 +0100

    Poprawne uruchamianie projektu z poziomu mavena (trzeba wywołać javafx:run), dodanie niewersjonowanych plików potrzebnych do testów

--- src/main/java/org/pl/model/AESFrame.java
+++ src/main/java/org/pl/model/AESFrame.java
@@ -6,12 +6,15 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.util.Objects;
+
 public class AESFrame extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
         Parent start = FXMLLoader.load(
-                getClass().getResource("/resources/AESFrame.fxml"));
+                Objects.requireNonNull(
+                        getClass().getResource("AESFrame.fxml")));
         stage.setTitle("AES");
         stage.setScene(new Scene(start, 727, 497));
         stage.show();
